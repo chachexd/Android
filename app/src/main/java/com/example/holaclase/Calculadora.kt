15d7d2779905bc8e07ab380ec11c406714d8e70a
diego <diegocuesta05@gmail.com>
2024-10-07 19:22:18 +0200

    Clase2android

--- app/src/main/java/com/example/holaclase/Calculadora.kt
+++ app/src/main/java/com/example/holaclase/Calculadora.kt
@@ -3,79 +3,96 @@ package com.example.holaclase
 import java.util.Scanner
 
 
-object Calculadora {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        var scanner = Scanner(System.`in`)
-        var salir = true;
 
-        while (salir) {
-            // Mensaje de bienvenida
-            println("Bienvenido a la calculadora básica")
+fun main() {
+    var scanner = Scanner(System.`in`)
+    var salir = true;
+
+    while (salir) {
+        // Mensaje de bienvenida
+        println("Bienvenido a la calculadora básica")
 
 
-            // Solicitar números al usuario
-            print("Ingrese el primer número: ")
-            var num1 = scanner.nextDouble()
+        // Solicitar números al usuario
+        print("Ingrese el primer número: ")
+        var num1 = scanner.nextDouble()
 
-            print("Ingrese el segundo número: ")
-            var num2 = scanner.nextDouble()
+        print("Ingrese el segundo número: ")
+        var num2 = scanner.nextDouble()
 
-            // Mostrar las operaciones disponibles
-            println("Seleccione la operación:")
-            println("1. Sumar")
-            println("2. Restar")
-            println("3. Multiplicar")
-            println("4. Dividir")
-            println("5. Salir")
+        // Mostrar las operaciones disponibles
+        println("Seleccione la operación:")
+        println("1. Sumar")
+        println("2. Restar")
+        println("3. Multiplicar")
+        println("4. Dividir")
+        println("5. Salir")
 
-            print("Ingrese su opción (1-4): ")
-            var opcion = scanner.nextInt()
+        print("Ingrese su opción (1-4): ")
+        var opcion = scanner.nextInt()
 
-            var resultado = 0.0
-            var operacionValida = true
+        var resultado = 0.0
+        var operacionValida = true
 
-            when (opcion) {
-                1 -> {
-                    resultado = num1 + num2
-                    println("Resultado: $num1 + $num2 = $resultado")
-                }
-
-                2 -> {
-                    resultado = num1 - num2
-                    println("Resultado: $num1 - $num2 = $resultado")
-                }
-
-                3 -> {
-                    resultado = num1 * num2
-                    println("Resultado: $num1 * $num2 = $resultado")
-                }
-
-                4 ->                 // Comprobar si se está dividiendo por cero
-                    if (num2 != 0.0) {
-                        resultado = num1 / num2
-                        println("Resultado: $num1 / $num2 = $resultado")
-                    } else {
-                        println("Error: No se puede dividir por cero.")
-                        operacionValida = false
-                    }
-                5 -> {
-                    salir = false
-                    println("Gracias por utilizar la calculadora. Adiós.")
-                }
-                else -> {
-                    println("Opción inválida. Por favor seleccione una opción del 1 al 4.")
-                    operacionValida = false
-                }
+        when (opcion) {
+            1 -> {
+                resultado = num1 + num2
+                println("Resultado: $num1 + $num2 = $resultado")
             }
 
-            // Mensaje final
-            if (operacionValida) {
-                println("Operación completada.")
+            2 -> {
+                resultado = num1 - num2
+                println("Resultado: $num1 - $num2 = $resultado")
+            }
+
+            3 -> {
+                resultado = num1 * num2
+                println("Resultado: $num1 * $num2 = $resultado")
+            }
+
+            4 ->                 // Comprobar si se está dividiendo por cero
+                if (num2 != 0.0) {
+                    resultado = num1 / num2
+                    println("Resultado: $num1 / $num2 = $resultado")
+                } else {
+                    println("Error: No se puede dividir por cero.")
+                    operacionValida = false
+                }
+            5 -> {
+                salir = false
+                println("Gracias por utilizar la calculadora. Adiós.")
+            }
+            else -> {
+                println("Opción inválida. Por favor seleccione una opción del 1 al 4.")
+                operacionValida = false
             }
         }
 
+        // Mensaje final
+        if (operacionValida) {
+            println("Operación completada.")
+        }
 
-        scanner.close()
     }
+    fun celsiusFahrenheit(n1 : Double) : Double {
+        return 9/5 * n1 + 32
+    }
+    fun fahrenheitKelvin(n1 : Double) : Double {
+        return 5/9 * (n1 - 32) + 273.15
+    }
+    fun kelvinCelsius(n1 : Double) : Double {
+        return n1 - 273.15
+    }
+    fun celsiusKelvin(n1 : Double) :Double{
+        return celsiusFahrenheit(fahrenheitKelvin(n1))
+    }
+    fun fahrenheitCelsius(n1 : Double) : Double {
+        return kelvinCelsius(fahrenheitKelvin(n1))
+    }
+    fun kelvinFahrenheit(n1 : Double) : Double {
+        return celsiusFahrenheit(kelvinCelsius(n1))
+    }
+
+    scanner.close()
 }
+
